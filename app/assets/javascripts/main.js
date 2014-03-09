997ed0293de3d8c3baa605d06c13ca5645b5ad21
Brandon Hays <brandon.hays@gmail.com>
2014-03-09 16:04:45 -0500

    handle network and validation errors

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
   $('#toggle-post-dialog').on("click", function(e) {
     e.preventDefault();
+    $("#new-gif-body").val("");
     $('#share-section .button-area').hide('fade');
     $('#gif-post-dialog').show('blind');
-    $("#gif-post-dialog button.gif-submit").attr("disabled", "disabled").show();
+    $("#gif-post-dialog button.gif-submit").attr("disabled", "disabled");
   });
 
   $('#gif-post-dialog button.cancel-post').on("click", function(e) {
@@ -21,7 +22,7 @@ $(document).ready(function(){
       charCount = ($(this).val().length - parsedUrl[0].length)
       if (charCount <= 140) {
         $("#gif-post-dialog button.gif-submit").removeAttr("disabled");
-        $('#gif-post-dialog .message').text("").removeClass("validation-error").hide();
+        $('#gif-post-dialog .message').text("").removeClass("error").hide();
         $('#gif-post-dialog .gif-preview-container').html('<div class="gif-preview"><img src="' + parsedUrl[0] + '" width="90"></div>')
      } else {
        $('#gif-post-dialog .message').show().addClass("validation-error").text("Your message is too long.");
@@ -39,7 +40,7 @@ $(document).ready(function(){
   $('#gif-post-dialog button.gif-submit').on("click", function(e) {
     e.preventDefault();
     var currentUserId = $('meta[name="current-user-id"]').attr("content");
-    $(this).hide("fade");
+    $(this).attr("disabled", "disabled");
     var url = $(this).attr('href');
     var body = $("#new-gif-body").val();
     var parsedUrl = body.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
@@ -58,17 +59,28 @@ $(document).ready(function(){
       var url = post.url || "";
       var username = (!!post.user && !!post.user.username) ? post.user.username : "";
       var body = post.body || null;
+      $('#gif-post-dialog .message').removeClass("error").text("");
       $('.share-gif-form').hide('fade');
       $("#gif-post-dialog .character-count").text("0");
-      $('#gif-post-dialog').append("<p class='success-text'>New gif posted: " + post.url + "</p>");
+      $('#gif-post-dialog .message').show().addClass("success").text("New gif posted: " + post.url);
       var newRow = '<td>' + username + '</td><td><img src="' + url + '"></td><td>' + body + '</td><td><a href="/gif_posts/' + post.id + '">Show</a></td><td><a href="/gif_posts/' + post.id + '/edit">Edit</a></td><td><a data-confirm="Are you sure?" data-method="delete" href="/gif_posts/' + post.id + '" rel="nofollow">Destroy</a></td>';
       $('table.gif-list tbody').prepend(newRow);
       setTimeout(function() {
         $('#share-section .button-area').show('fade');
-        $('#gif-post-dialog .success-text').remove();
+        $('#gif-post-dialog .message').hide().text("");
         $('#gif-post-dialog .gif-preview').remove()
         $('#gif-post-dialog').hide('blind');
         $('.share-gif-form').show();
+      }, 5000);
+    }).fail(function(data) {
+      if (!data.responseText) {
+        $('#gif-post-dialog .message').show().addClass("error").text("There was an error posting your gif. Please wait and try again.");
+      } else {
+        $('#gif-post-dialog .message').show().addClass("error").text(data.responseJSON.errors.url[0]);
+      }
+      setTimeout(function() {
+        $('#gif-post-dialog .message').removeClass("error").hide();
+        $("#gif-post-dialog button.gif-submit").removeAttr("disabled");
       }, 5000);
     });
   });
--- app/models/gif_post.rb
+++ app/models/gif_post.rb
@@ -1,3 +1,5 @@
 class GifPost < ActiveRecord::Base
   belongs_to :user
+  validates :body, presence: true
+  validates :url, presence: true, uniqueness: { scope: :user_id, message: "You have already posted this image. Please pick a different image and try again." }
 end
