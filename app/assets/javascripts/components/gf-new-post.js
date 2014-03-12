c913b11aa5add8135b7b73098e6c23766ed5720d
Brandon Hays <brandon.hays@gmail.com>
2014-03-11 19:03:24 -0500

    move save function to model

--- app/assets/javascripts/components/gf-new-post.js
+++ app/assets/javascripts/components/gf-new-post.js
@@ -1,6 +1,10 @@
 App.GifPost = Ember.Object.extend({
+  /* PROPERTIES */
   regex: gifRegex(),
   body: "",
+  urlRoot: "/gif_posts",
+  username: "",
+
   parsedUrl: function() {
     var matches = this.get("body").match(this.get("regex"))
     return (matches && matches.length > 0) ? matches[0] : "";
@@ -17,15 +21,31 @@ App.GifPost = Ember.Object.extend({
   }.property("body", "parsedUrl", "isGif"),
   isValid: function() {
     return !!(this.get("charCount") <= 140 && this.get("isGif"))
-  }.property("charCount", "isGif")
+  }.property("charCount", "isGif"),
+
+  /* FUNCTIONS */
+  save: function() {
+    return ic.ajax({
+      type: "POST",
+      dataType: "json",
+      url: this.get("urlRoot"),
+      data: {
+        gif_post: {
+          body: this.get("body"),
+          url: this.get("parsedUrl"),
+        }
+      }
+    })
+  }
 });
 
 App.GfNewPostComponent = Ember.Component.extend({
+  /* PROPERTIES */
   currentUser: null,
   layoutName: "components/gf-new-post",
   gifPost: null,
-  postUrl: "/gif_posts",
 
+  /* OBSERVERS */
   initLegacyCode: function() {
     $('section.gif-list').on("click", "article a[data-gif-delete]", function(e) {
       e.preventDefault();
@@ -58,6 +78,7 @@ App.GfNewPostComponent = Ember.Component.extend({
     }
   }.observes("gifPost.body", "gifPost.isValid"),
 
+  /* ACTIONS */
   actions: {
     showDialog: function() {
       $("#new-gif-body").val("");
@@ -71,27 +92,16 @@ App.GfNewPostComponent = Ember.Component.extend({
       $('#gif-post-dialog').hide();
     },
     submit: function() {
-      var currentUserId = $('meta[name="current-user-id"]').attr("content");
-      $(this).attr("disabled", "disabled");
-      ic.ajax({
-        type: "POST",
-        dataType: "json",
-        url: this.get("postUrl"),
-        data: {
-          gif_post: {
-            body: this.get("postUrl.body"),
-            url: this.get("gifPost.parsedUrl"),
-          }
-        }
-      }).then(function(data) {
-        var post = data.gif_post;
-        var url = post.url || "";
-        var username = (!!post.user && !!post.user.username) ? post.user.username : "";
-        var body = post.body || null;
+      var gifPost = this.get("gifPost")
+      this.$("a.gif-submit").attr("disabled", "disabled");
+      gifPost.save().then(function(data) {
+        // Success
+        gifPost.set("username", data.gif_post.user.username)
+        gifPost.set("id", data.gif_post.id)
         $('#gif-post-dialog .message').removeClass("error").text("");
         $('.share-gif-form').hide('fade');
-        $('#gif-post-dialog .message').show().addClass("success").text("New gif posted: " + post.url);
-        var newArticle = '<article class="gif-entry" data-gif-entry data-gif-post-id="' + post.id + '"><div class="gif-entry-image"><img class="framed" src="' + url + '"></div><div class="gif-entry-body">' + body + '</div><div class="gif-entry-delete"><a class="btn btn-danger"data-gif-delete data-gif-post-id="' + post.id + '" href="/gif_posts/' + post.id + '" rel="nofollow">Delete</a></div><div class="gif-entry-user">Shared by ' + username + '</div><div class="gif-entry-permalink"><a href="/gif_posts/' + post.id + '">Permalink</a></div><div style="clear:both;"></div></article>';
+        $('#gif-post-dialog .message').show().addClass("success").text("New gif posted: " + gifPost.get("parsedUrl"));
+        var newArticle = '<article class="gif-entry" data-gif-entry data-gif-post-id="' + gifPost.get("id") + '"><div class="gif-entry-image"><img class="framed" src="' + gifPost.get("parsedUrl") + '"></div><div class="gif-entry-body">' + gifPost.get("body") + '</div><div class="gif-entry-delete"><a class="btn btn-danger"data-gif-delete data-gif-post-id="' + gifPost.get("id") + '" href="/gif_posts/' + gifPost.get("id") + '" rel="nofollow">Delete</a></div><div class="gif-entry-user">Shared by ' + gifPost.get("username") + '</div><div class="gif-entry-permalink"><a href="/gif_posts/' + gifPost.get("id") + '">Permalink</a></div><div style="clear:both;"></div></article>';
         $('section.gif-list').prepend(newArticle);
         setTimeout(function() {
           console.log("running set-timeout on success")
@@ -100,6 +110,7 @@ App.GfNewPostComponent = Ember.Component.extend({
           $('#gif-post-dialog').hide('blind');
           $('.share-gif-form').show();
         }, 5000);
+      // Failure
       }, function(data) {
         $('#gif-post-dialog .message').removeClass("success").text("");
         if (!data.jqXHR || !data.jqXHR.responseJSON) {
@@ -116,6 +127,7 @@ App.GfNewPostComponent = Ember.Component.extend({
   }
 });
 
+/* INITIALIZATION */
 $(document).ready(function() {
   $("#new-post-container").each(function(){
     var component = App.GfNewPostComponent.create({
